e26b2294dcae148b6d5e9e3b8fcad6df35237394
Titus Soporan <titus@tsoporan.com>
2019-01-01 20:22:48 -0500

    Basic input loop
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,8 +1,27 @@
-﻿// BookTracker
+﻿open System
 
-open System
+
+let rec readInput () =
+    printfn "What would you like to do?"
+
+    match stdin.ReadLine() with
+        | "quit" -> 0
+        | _ -> readInput()
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0
+    let msg = """
+______             _  _____         _             
+| ___ \           | ||_   _|       | |            
+| |_/ / ___   ___ | | _| |_ __ __ _| | _____ _ __ 
+| ___ \/ _ \ / _ \| |/ / | '__/ _` | |/ / _ \ '__|
+| |_/ / (_) | (_) |   <| | | | (_| |   <  __/ |   
+\____/ \___/ \___/|_|\_\_/_|  \__,_|_|\_\___|_|   
+
+Welcome to BookTraker a simple console based book tracking system!
+    """
+    printfn "%s" msg
+
+    let input = readInput()
+    input
